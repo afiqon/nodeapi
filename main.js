2ec6c368a24f7474b7bcef1bf0252c372b9cf337
Muhammad Afiq <afiqon@gmail.com>
2018-09-05 23:32:26 +0800

    added login

--- main.js
+++ main.js
@@ -58,6 +58,45 @@ app.get('/subjek', function (req, res) {
     });
 });
 
+//Login
+app.post('/login', function (req, res) {
+    var email= req.body.email;
+    var password = req.body.password;
+    connection.query('SELECT * FROM pengajar WHERE email_pengajar = ?',[email], function (error, results, fields) {
+    console.log(req.body);
+    if (error) {
+      // console.log("error ocurred",error);
+      res.send({
+        "code":400,
+        "failed":"error ocurred"
+      })
+    }else{
+      // console.log('The solution is: ', results);
+      if(results.length >0){
+        if(results[0].pass_pengajar == password){
+          res.send({
+            "code":200,
+            "success":"login sucessfull",
+            "name": `${results[0].nama_pengajar}`
+              });
+        }
+        else{
+          res.send({
+            "code":204,
+            "success":"Email and password does not match"
+              });
+        }
+      }
+      else{
+        res.send({
+          "code":204,
+          "success":"Email does not exits"
+            });
+      }
+    }
+    });
+});
+
 
 //find_user_by_id
 app.get('/user_id', function (req, res) {
